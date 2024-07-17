c3b2b1416e0642c73711dcca363a4a69d9714379
Developers Today <mshari7185@gmail.com>
2024-07-17 22:31:36 +0530

    patient related routes

---
+++ server/routes/patientRoute.js
@@ -0,0 +1,12 @@
+// doctorRoutes.js
+const express = require('express');
+const router = express.Router();
+const patientController = require('../controllers/patientController');
+const authMiddleware = require('../middleware/patientMiddleware.js');
+
+// Route definitions using middleware
+router.post('/signup', patientController.signup);
+router.post('/login', patientController.login);
+router.post('/doctor-details',authMiddleware, patientController.getDoctorDetails);
+
+module.exports = router;
